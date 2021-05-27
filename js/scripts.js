42b5a5235607b9fdbdb7ff0d51dd182ce07172eb
biancatron <84161563+biancatron@users.noreply.github.com>
2021-05-26 23:44:07 -0500

    Added JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Welcome! Enjoy your stay.")
+
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
